df362ad6d02a263217af0e9e1c259d91236b3943
gh0stsh0t <rjperias0213@gmail.com>
2017-03-21 11:20:19 +0800

    finished deactivate and view members

---
+++ WindowsFormsApplication2/pendingRequests.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class pendingRequests
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Approve";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 36);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Reject";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 36);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(397, 326);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // pendingRequests
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 384);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "pendingRequests";
+            this.Text = "pendingRequests";
+            this.Load += new System.EventHandler(this.pendingRequests_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ WindowsFormsApplication2/pendingRequests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class pendingRequests : Form
+    {
+
+        Members upper;
+        MySqlConnection conn;
+        public pendingRequests(Members x)
+        {
+            InitializeComponent();
+            upper = x;
+        }
+
+        private void pendingRequests_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlCommand comm = new MySqlCommand("SELECT members.family_name,member.first_name,loanrequest.amount,loanrequest.purpose,loanrequest.approval_no FROM members,loanRequest where RequestStatus=0", conn);
+                MySqlDataAdapter listener = new MySqlDataAdapter(comm);
+                DataTable table = new DataTable();
+                listener.Fill(table);
+                dataGridView1.DataSource = table;
+                dataGridView1.Columns["family_name"].HeaderText = "Family Name";
+                dataGridView1.Columns["first_name"].HeaderText = "Fir.st Name";
+                dataGridView1.Columns["amount"].HeaderText = "Amount";
+                dataGridView1.Columns["purpose"].HeaderText = "Purpose";
+                dataGridView1.Columns["approval_no"].Visible = false;
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                    column.SortMode = DataGridViewColumnSortMode.NotSortable;
+                conn.Close();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message + " also, just let it run. data validation is TODO");
+                conn.Close();
+            }
+        }
+    }
+}
