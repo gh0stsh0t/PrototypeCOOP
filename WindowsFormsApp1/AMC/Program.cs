6458d9494c60003b670d0f3a7caf4d0901eeb124
gh0stsh0t <rjperias0213@gmail.com>
2017-07-07 11:47:04 +0800

    created view members form

--- WindowsFormsApp1/AMC/Program.cs
+++ WindowsFormsApp1/AMC/Program.cs
@@ -16,7 +16,7 @@ namespace AMC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AddTransaction());
+            Application.Run(new ViewMember());
         }
     }
 }
