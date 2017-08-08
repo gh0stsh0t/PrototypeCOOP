4d48ebf5c1679083550a3458fc63571de2ae543b
gh0stsh0t <rjperias0213@gmail.com>
2017-08-08 11:29:34 +0800

    non working

---
+++ WindowsFormsApp1/AMC/databaseConn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AMC
+{
+    class databaseConn
+    {
+        private DataTable holder;
+        private MySqlConnection databasecon;
+        private MySqlDataAdapter listener;
+        //private MySqlCommand query;
+        public databaseConn()
+        {
+            databasecon = new MySqlConnection("Server=localhost;Database=amc;Uid=root;Pwd=root;");
+        }
+
+        public DataTable getData()
+        {
+            return holder;
+        }
+
+        public dynamic query(int type, string table, string fields, string where)
+        {
+            switch (type)
+            {
+                case 1:
+                    return query(table, fields, where);
+                case 2:
+                    query(table, fields,where);
+                    break;
+                case 3:
+                    query();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public DataTable query(string table, string fields,string where)
+        {
+            string queryHolder = "SELECT "+ string.Join(", ", fields.Split(' '))+" FROM "+table;
+
+        }
+
+        public bool query(string tables, string fields,string where)
+        {
+            
+        }
+    }
+}
